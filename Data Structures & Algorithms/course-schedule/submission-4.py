c74a6ca31aca8ed8fead240c0c02180e1c1e4c03
master-senses <151479862+master-senses@users.noreply.github.com>
2026-05-13 22:35:55 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {}
+        for i in range(numCourses):
+            adj[i] = []
+
+        for pair in prerequisites:
+            adj[pair[0]].append(pair[1])
+        
+        def dfs(visited, node):
+            if len(adj[node]) == 0:
+                return True
+            if node in visited:
+                print("node is in visited", visited, node)
+                return False
+            visited.add(node)
+            
+            for i in adj[node]:
+                if not dfs(visited, i):
+                    # print(node)
+                    return False
+            visited.remove(node)
+            return True
+        print(adj)
+        for i in range(numCourses):
+            visited = set()
+            if not dfs(visited, i):
+                return False
+        
+        return True
+
+
+                
