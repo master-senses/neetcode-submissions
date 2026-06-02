2c4d358639ed01bf4656bfd5056bb6e7335738bc
master-senses <151479862+master-senses@users.noreply.github.com>
2026-06-01 22:01:35 -0400

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        # visited = set()
+        rowlen = len(board)
+        collen = len(board[0])
+
+        def dfs(i, j):
+            if i < 0 or i >= rowlen or j < 0 or j >= collen:
+                return
+            if board[i][j] == "X":
+                return
+            if board[i][j] == "T":
+                return
+            board[i][j] = "T"
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+            return
+
+        def isvalid(i, j):
+            if i == 0 or (i == rowlen - 1) or j == 0 or (j == collen  - 1):
+                return True
+            return False
+        
+        for i in range(rowlen):
+            for j in range(collen):
+                if isvalid(i, j):
+                    dfs(i, j)
+        # print(visited)
+        for i in range(rowlen):
+            for j in range(collen):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "T":
+                    board[i][j] = "O"
+                
+    
