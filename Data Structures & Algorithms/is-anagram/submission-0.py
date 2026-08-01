272064d1393bb7e1cfd308d7c99328622538cc7c
master-senses <151479862+master-senses@users.noreply.github.com>
2026-08-01 10:45:58 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        str1 = Counter(s)
+        str2 = Counter(t)
+
+        return str1 == str2
