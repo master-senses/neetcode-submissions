2d7f1a01a33a0933663ea8abfd440b5bd524d9d8
master-senses <151479862+master-senses@users.noreply.github.com>
2026-08-06 17:17:56 -0400

    Add: course-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-4.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        cycle = set()
+        path = []
+        visited = set()
+        adj = defaultdict(list)
+        roots = set(i for i in range(numCourses))
+
+        for i in prerequisites:
+            if i[1] in roots:
+                roots.remove(i[1])
+            adj[i[0]].append(i[1])
+        
+        if not roots:
+            # print(adj)
+            return []
+        print(roots)
+        print(adj)
+        # print(adj)
+        def dfs(node):
+            # global path
+            if node in cycle:
+                return False
+            if node in visited:
+                return True
+            cycle.add(node)
+            for i in adj[node]:
+                if not dfs(i):
+                    return False
+            path.append(node)
+            adj[node] = []
+            cycle.remove(node)
+            visited.add(node)
+            return True
+        
+        for i in roots:
+            if not dfs(i):
+                return []
+        
+        return path
+        
+        
+        
+
+        
+        
